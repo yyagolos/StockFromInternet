83c6ce5d8d974e52c1767e6932e8b31e06661bb4
Dell <Dell@DESKTOP-09QFUKB>
2023-07-23 04:57:27 +0300

    Добавьте файлы проекта.

---
+++ StockFromInternet/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using StockFromInternet;
+
+class Program
+{
+     static void Main(string[] args)
+    {
+        StockExchange StockExchange = new StockExchange();
+        StockExchange.PriceChangeHandler = ShowPrice;
+        StockExchange.Start();
+    }
+    public static void ShowPrice(int Price) => Console.WriteLine($"New price is:{Price}");
+}
---
+++ StockFromInternet/StockExchange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFromInternet
+{
+    public class StockExchange
+    {
+        public delegate void PriceChange (int price);
+        public PriceChange PriceChangeHandler { get; set; }
+
+        public void Start()
+        {
+            while (true) 
+            {
+            int SberbankPrice  = (new Random()).Next(50,100);
+                PriceChangeHandler(SberbankPrice);
+                Thread.Sleep(2000);
+
+
+            }
+
+        }
+
+    }
+}
